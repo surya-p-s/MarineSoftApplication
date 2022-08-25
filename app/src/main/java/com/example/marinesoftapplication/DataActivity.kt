ed9111bd3ab7962893da50ae4746e96d8bc4e234
surya-p-s <surya3011singh@gmail.com>
2022-08-25 23:47:51 +0530

    navigation bar

--- app/src/main/java/com/example/marinesoftapplication/DataActivity.kt
+++ app/src/main/java/com/example/marinesoftapplication/DataActivity.kt
@@ -1,46 +1,48 @@
 package com.example.marinesoftapplication
 
+import android.content.ClipData
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class DataActivity : AppCompatActivity() {
+
     private lateinit var auth : FirebaseAuth
-//    private lateinit var drawblelayout:DrawerLayout
-//    private lateinit var navigationview:NavigationView
-//    private lateinit var toolbar:Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data)
 
-        // Authentiation
         auth = Firebase.auth
-        val logOut=findViewById<FloatingActionButton>(R.id.logOut)
-        logOut.setOnClickListener{
+
+        val home=findViewById<NavigationBarItemView>(R.id.home_menu)
+         home.setOnClickListener {
+             startActivity(Intent(this,DataActivity::class.java))
+         }
+
+        val signOut=findViewById<NavigationBarItemView>(R.id.signOut)
+        // Authentiation
+        signOut.setOnClickListener{
             Firebase.auth.signOut()
             updateUI()
         }
-        val uploadButton=findViewById<FloatingActionButton>(R.id.addpicButton)
-
+        val uploadButton=findViewById<NavigationBarItemView>(R.id.imageUpload)
+//
         uploadButton.setOnClickListener {
             val intent=Intent(this,UploadActivity::class.java)
             startActivity(intent)
         }
-
-        // Menu Layout
-//        drawblelayout=findViewById(R.id.drawablelayout)
-//        navigationview=findViewById(R.id.navigationView)
-//        toolbar=findViewById(R.id.toolBar)
-
 
     }
 
--- app/src/main/java/com/example/marinesoftapplication/UploadActivity.kt
+++ app/src/main/java/com/example/marinesoftapplication/UploadActivity.kt
@@ -8,6 +8,10 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
+import com.google.android.material.navigation.NavigationBarItemView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class UploadActivity : AppCompatActivity() {
 
@@ -16,25 +20,54 @@ class UploadActivity : AppCompatActivity() {
     private val pickImage = 100
     private var imageUri: Uri?=null
 
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+
+        auth = Firebase.auth
+
         title="MarineSoft"
-        imageView=findViewById(R.id.imageUpload)
+        imageView=findViewById(R.id.imageUploadView)
         button=findViewById(R.id.selectImgButton)
 
         button.setOnClickListener {
             val gallery=Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
             startActivityForResult(gallery, pickImage)
         }
+
+        val home=findViewById<NavigationBarItemView>(R.id.home_menu)
+        home.setOnClickListener {
+            startActivity(Intent(this,DataActivity::class.java))
+        }
+
+        val signOut=findViewById<NavigationBarItemView>(R.id.signOut)
+        // Authentiation
+        signOut.setOnClickListener{
+            Firebase.auth.signOut()
+            updateUI()
+        }
+        val uploadButton=findViewById<NavigationBarItemView>(R.id.imageUpload)
+//
+        uploadButton.setOnClickListener {
+            val intent=Intent(this,UploadActivity::class.java)
+            startActivity(intent)
+        }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode== RESULT_OK && requestCode==pickImage){
             imageUri=data?.data
             imageView.setImageURI(imageUri)
         }
+    }
+    private fun updateUI() {
+        val intent= Intent(this,SignInPage::class.java)
+        startActivity(intent)
+        finish()
     }
 
 }
